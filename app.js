3f2363885214b3535ca8cd3b2b31fe7bc505eece
brantx <brantx@163.com>
2018-10-20 22:36:33 +0800

    add uploading function

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var markdown = require('markdown').markdown;
 var cookieParser = require('cookie-parser');
+var multiparty = require('multiparty');
+var util = require('util');
+var fs = require('fs');
+
+
 const shortid = require('shortid')
 
 var bodyParser = require('body-parser');
@@ -36,12 +41,52 @@ app.get('/', function(req, res){
    res.redirect('/home');
 });
 
+
+/* Pic 上传*/
+app.post('/admin/upload', function(req, res, next){
+  //生成multiparty对象，并配置上传目标路径
+  var form = new multiparty.Form({uploadDir: './upload/'});
+  //上传完成后处理
+  form.parse(req, function(err, fields, files) {
+    var filesTmp = JSON.stringify(files,null,2);
+
+    if(err){
+      console.log('parse error: ' + err);
+    } else {
+      console.log('parse files: ' + filesTmp);
+      var inputFile = files.inputFile[0];
+      var uploadedPath = inputFile.path;
+      var dstPath = './upload/' + inputFile.originalFilename;
+      //重命名为真实文件名
+      fs.rename(uploadedPath, dstPath, function(err) {
+        if(err){
+          console.log('rename error: ' + err);
+        } else {
+          console.log('rename ok');
+        }
+      });
+    }
+
+    res.writeHead(200, {'content-type': 'text/plain;charset=utf-8'});
+    res.write('received upload:\n\n');
+    res.end(util.inspect({fields: fields, files: filesTmp}));
+ });
+});
+
+app.get('/admin/upload',function(req, res, next){
+	fs.readdir('./upload',function(err,files)
+	{
+		console.log(files);
+	});
+  res.render('upload');
+});
+
 app.all('/admin/pwd',function(req,res){
   if(req.body.pwd && req.body.oldpwd && (req.body.pwd == req.body.pwd2) && req.body.oldpwd == db.get("passwd").value()){
 	  db.find("passwd")
 			.assign({'passwd':req.body.pwd})
 			.write();
-		res.send("密码更新成功！");
+		res.render('redirect',{msg:"密码更新成功！",url:'/admin/'});
 	}
 	res.render('pwd');
 });
